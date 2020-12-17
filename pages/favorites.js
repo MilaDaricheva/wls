6012965559965b3ac8bc81a753cbcda4d5cd9d9e
Mila Daricheva <miladaricheva@gmail.com>
2020-12-17 10:13:58 -0500

    favorites content

--- pages/favorites.js
+++ pages/favorites.js
@@ -20,6 +20,22 @@ export default function Favorites() {
           {siteTitle}
         </h1>
 
+        <p>Why Jamstack?</p>
+
+        <p>Jamstack is an architecture designed to make the web faster, more secure, and easier to scale with help of the following technologies:</p>
+
+        <p>Headless CMS makes content accessible via an API for display on any device.</p>
+
+        <p>REST APIs give us flexibility.</p>
+
+        <p>Cloud computing provides scalability.</p>
+
+        <p>Next.js optimizes your application for the best performance by code splitting, client-side navigation, and prefetching data.</p>
+
+        <p>Ghost is very fast and lightweight headless CMS, excellent choice for blogging. </p>
+
+        <p>Strapi CMS is a wonderful choice for e - commerce solutions and other more complex applications. </p >
+
       </main>
 
 
