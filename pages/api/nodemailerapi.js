48f568cb9b74df9bb26f9e460ce788cf02097d3b
Mila Daricheva <miladaricheva@gmail.com>
2020-12-29 21:41:49 -0500

    server side form validation

--- pages/api/nodemailerapi.js
+++ pages/api/nodemailerapi.js
@@ -1,4 +1,11 @@
 import nodemailer from "nodemailer"
+import * as yup from 'yup';
+
+let schema = yup.object().shape({
+  name: yup.string("name").required("name"),
+  mail: yup.string("mail").required("mail").email("mail"),
+  formContent: yup.string("formContent").required("formContent")
+});
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -11,18 +18,18 @@ const transporter = nodemailer.createTransport({
 export default async (req, res) => {
 
   const { name, mail, formContent } = req.body
-
+  const fields = { name, mail, formContent }
   // Check if fields are all filled 
   //check for undefined
-  if (name === "" || mail === "" || formContent === "") {
+  if (schema.isValidSync(fields)) {
+    const messageBody = `${name} ${mail} ${formContent}`;
+
+    const mailerRes = await mailer(messageBody)
+    res.send(mailerRes)
+  } else {
     res.status(403).send("")
     return
   }
-
-  const messageBody = `${name} ${mail} ${formContent}`;
-
-  const mailerRes = await mailer(messageBody)
-  res.send(mailerRes)
 
 }
 
