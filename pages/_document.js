3dd65dd1d7c0afc77ef3f280fce133ac07f73fb6
Mila Daricheva <miladaricheva@gmail.com>
2020-12-31 13:11:12 -0500

    favicon fix

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,11 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <link rel="icon" type="image/x-icon" href="../static/favicon.ico" />
+        </Head>
         <body>
           <Main />
           <NextScript />
