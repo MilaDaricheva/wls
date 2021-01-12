95b0c9ad0a74243478555fb4cd96e3197e9d06ee
Mila Daricheva <miladaricheva@gmail.com>
2021-01-12 17:45:42 -0500

    CU-j2vwf3[closed] page not found

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+export default function Custom404() {
+  return (
+    <div className="mainWrap">
+      <div>
+        <main>
+          <h1>404 - Page Not Found</h1>
+        </main>
+      </div>
+    </div>
+  )
+}
