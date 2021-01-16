a25bd20141483332b200cd7846cc9a3bfef8891a
Mila Daricheva <miladaricheva@gmail.com>
2021-01-15 20:40:37 -0500

    make styles inline

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,24 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-
+class InlineStylesHead extends Head {
+  getCssLinks({ allFiles }) {
+    return allFiles
+      .filter((file) => file.endsWith('.css'))
+      .map((file) => (
+        <style
+          key={file}
+          nonce={this.props.nonce}
+          dangerouslySetInnerHTML={{
+            __html: fs.readFileSync(path.join('.next', file), 'utf-8'),
+          }}
+        />
+      ));
+  }
+}
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <InlineStylesHead />
         <body>
           <Main />
           <NextScript />
